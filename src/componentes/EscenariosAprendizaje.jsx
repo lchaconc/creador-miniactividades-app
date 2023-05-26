c51b356f0a974e676d7c9fb7ff815590c8b00dd6
lchaconc <luis.chacon.campos@gmail.com>
2023-05-26 14:51:43 -0600

    modal rezise
    -Carga de modal redimension
    -renderizado de img previa
    -estado de carga tipo modal

--- src/componentes/EscenariosAprendizaje.jsx
+++ src/componentes/EscenariosAprendizaje.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Modal from "react-bootstrap/Modal";
 import addDragAndDrop from "../utils/draggable";
 
@@ -10,10 +10,13 @@ export default function EscenariosAprendizaje(props) {
   const [imgFondo, setImgFondo] = useState(null);
   const [sprites, setSprites] = useState([]);
   const [tipoModal, setTipoModal] = useState(null);
+  const [spriteActual, setSpriteActual] = useState(null);
+
+  const refMenuContextual = useRef(null);
 
   useEffect(() => {
-    console.log(sprites);
-  }, [sprites]);
+    console.log(tipoModal);
+  }, [tipoModal]);
 
   const handleClose = () => setShow(false);
 
@@ -48,9 +51,29 @@ export default function EscenariosAprendizaje(props) {
   }
 
   const handleAbrirModal = (e) => {
+    console.log("Abrindo modal en modo", tipoModal);
+    refMenuContextual.current.style.display = "none";
     setTipoModal(e.target.dataset.modal);
     setShow(true);
-    setImgPrevia(null);
+    if (tipoModal === "redim") {
+      console.log("spriteActual", spriteActual);
+      setImgPrevia(spriteActual);
+    } else {
+      setImgPrevia(null);
+    }
+  };
+
+  const handleAbrirMenucontextual = (e) => {
+    console.log(e.target);
+    setSpriteActual(sprites[0].sprite);
+    e.preventDefault();
+    refMenuContextual.current.style.display = "block";
+    refMenuContextual.current.style.left = e.pageX + "px";
+    refMenuContextual.current.style.top = e.pageY + "px";
+  };
+
+  const handleCerrarMenu = () => {
+    refMenuContextual.current.style.display = "none";
   };
 
   const estilofondo = {
@@ -82,7 +105,7 @@ export default function EscenariosAprendizaje(props) {
             </button>
             <button
               id="btnSprite"
-              data-Modal="sprite"
+              data-modal="sprite"
               onClick={handleAbrirModal}
               className="btn btn-success"
             >
@@ -94,40 +117,73 @@ export default function EscenariosAprendizaje(props) {
 
         <div className="row">
           {imgFondo && (
-            <div id="divFondo" className="col-12" style={estilofondo}>
+            <div
+              id="divFondo"
+              onClick={handleCerrarMenu}
+              className="col-12"
+              style={estilofondo}
+            >
               {sprites &&
-                sprites.map(
-                  item => (
-                  <img 
-                  key={item.id} 
-                  onMouseDown={addDragAndDrop} 
-                  className="img-sprite"                  
-                  src={item.sprite} 
-                  alt="sprite" />)
-                )}
+                sprites.map((item) => (
+                  <img
+                    key={item.id}
+                    onMouseDown={addDragAndDrop}
+                    onContextMenu={handleAbrirMenucontextual}
+                    className="img-sprite"
+                    src={item.sprite}
+                    alt="sprite"
+                  />
+                ))}
             </div>
           )}
         </div>
       </div>
 
-      <Modal 
-      size={tipoModal === "fondo" ? "xl" : "sm"} 
-      show={show} onHide={handleClose}>
+      <Modal
+        size={tipoModal === "fondo" ? "xl" : "sm"}
+        show={show}
+        onHide={handleClose}
+      >
         <Modal.Header closeButton>
           <Modal.Title>
-            {tipoModal === "fondo" ? "Fondo" : "Sprite"  }
+            {tipoModal === "fondo" ? "Fondo" : "Sprite"}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <input type="file" id="inputFile" onChange={handleLoadFile} />
+          {(tipoModal === "fondo" || tipoModal === "sprite") && (
+            <div className="row">
+              <div className="col-12 text-center">
+                <input type="file" id="inputFile" onChange={handleLoadFile} />
+              </div>
+            </div>
+          )}
+
           <hr />
           {imgPrevia && (
-            <img
-              className="img-fluid"
-              id="imagePreview"
-              src={imgPrevia}
-              alt="Vista previa de la imagen"
-            ></img>
+            <div className="row">
+              <div className="col-12 text-center">
+                <img
+                  className="img-fluid"
+                  id="imagePreview"
+                  src={imgPrevia}
+                  alt="Vista previa de la imagen"
+                ></img>
+              </div>
+            </div>
+          )}
+
+          {tipoModal === "redim" && (
+            <>
+              <br />
+              <label for="customRange1" className="form-label">
+                Redimensión <strong>50%</strong>
+              </label>
+              <input
+                type="range"
+                className="form-range"
+                id="customRange1"
+              ></input>
+            </>
           )}
         </Modal.Body>
         <Modal.Footer>
@@ -136,6 +192,21 @@ export default function EscenariosAprendizaje(props) {
           </button>
         </Modal.Footer>
       </Modal>
+
+      <div id="menuContextual" ref={refMenuContextual} className="context-menu">
+        <ul className="context-menu__items">
+          <li className="context-menu__item">Eliminar 🗑️ </li>
+          <li
+            className="context-menu__item"
+            onClick={handleAbrirModal}
+            data-modal="redim"
+          >
+            Redimensionar 📐
+          </li>
+          <li className="context-menu__item">Editar acción 🎯 </li>
+          <li className="context-menu__item">Cambiar propiedades 🚧 </li>
+        </ul>
+      </div>
     </>
   );
 }
