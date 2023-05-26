0049264b57c9a6a30c5478656d79b318d3c1f6c5
lchaconc <luis.chacon.campos@gmail.com>
2023-05-26 11:11:57 -0600

    dif en carga de sprites y fondo
    -Imp de estados
    -caraga de fondo
    -Creación de array sprites
    -renderizado de sprites
    -despiegue de imf fondo en css

--- src/componentes/EscenariosAprendizaje.jsx
+++ src/componentes/EscenariosAprendizaje.jsx
@@ -1,39 +1,65 @@
 import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 
+let nextId = 0;
+
 export default function EscenariosAprendizaje(props) {
   const [show, setShow] = useState(false);
-  const [previaImg, setPreviaImg] = useState(null);
+  const [imgPrevia, setImgPrevia] = useState(null);
   const [imgFondo, setImgFondo] = useState(null);
-  
+  const [sprites, setSprites] = useState([]);
+  const [tipoModal, setTipoModal] = useState(null);
+
+  useEffect(() => {
+    console.log(sprites);
+  }, [sprites]);
 
   const handleClose = () => setShow(false);
-  
 
   const handleLoadFile = (e) => {
     const file = e.target.files[0];
     console.log(file);
-    //setIsBg(false);
 
     if (file) {
       const reader = new FileReader();
       reader.addEventListener("load", function () {
-        setPreviaImg(reader.result);
+        setImgPrevia(reader.result);
       });
 
       reader.readAsDataURL(file);
     }
   };
 
-  function handleAceptarfondo() {
-    setImgFondo(previaImg);    
-    setShow(false)
-    
+  function handeGuardarImg() {
+    if (tipoModal === "btnFondo") {
+      setImgFondo(imgPrevia);
+    }
+
+    if (tipoModal === "btnSprite") {
+      setSprites([
+        // with a new array
+        ...sprites, // that contains all the old items
+        { id: nextId++, sprite: imgPrevia }, // and one new item at the end
+      ]);
+    }
+
+    setShow(false);
   }
 
-  const handleShowMdLoadImg = ()=> {    
+  const handleAbrirModal = (e) => {
+    setTipoModal(e.target.id);
     setShow(true);
-  }
+    setImgPrevia(null);
+  };
+
+  const estilofondo = {
+    backgroundImage: `url(${imgFondo})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+    width: "100%",
+  };
 
   return (
     <>
@@ -45,28 +71,34 @@ export default function EscenariosAprendizaje(props) {
         </div>
         <div className="row">
           <div className="col-12">
-            <button id="btnFondo" className="btn btn-info" onClick={handleShowMdLoadImg}>              
-              Agreggar Imagen
+            <button
+              id="btnFondo"
+              className="btn btn-info"
+              onClick={handleAbrirModal}
+            >
+              Agreggar Fondo
             </button>
-            <button 
-            id="btnSprite"
-            onClick={handleShowMdLoadImg}
-            className="btn btn-success"> Agregar sprite </button>
+            <button
+              id="btnSprite"
+              onClick={handleAbrirModal}
+              className="btn btn-success"
+            >
+              {" "}
+              Agregar sprite{" "}
+            </button>
           </div>
         </div>
 
         <div className="row">
-            {
-                (imgFondo ) &&
-                <div className="col-12">
-                <img 
-                className="img-fluid"
-                src={imgFondo} alt="Fondo" />
+          {imgFondo && (
+            <div id="divFondo" className="col-12" style={estilofondo}>
+              {sprites &&
+                sprites.map(
+                  item => (<img key={item.id} src={item.sprite} alt="sprite" />)                  
+                )}
             </div>
-            }
+          )}
         </div>
-
-
       </div>
 
       <Modal size="lg" show={show} onHide={handleClose}>
@@ -76,19 +108,19 @@ export default function EscenariosAprendizaje(props) {
         <Modal.Body>
           <input type="file" id="inputFile" onChange={handleLoadFile} />
           <hr />
-          {previaImg && (
+          {imgPrevia && (
             <img
-                className="img-fluid"
+              className="img-fluid"
               id="imagePreview"
-              src={previaImg}
+              src={imgPrevia}
               alt="Vista previa de la imagen"
             ></img>
           )}
         </Modal.Body>
         <Modal.Footer>
-            <button 
-            onClick={handleAceptarfondo}
-            className="btn btn-outline-info" >Aceptar</button>
+          <button onClick={handeGuardarImg} className="btn btn-outline-info">
+            Aceptar
+          </button>
         </Modal.Footer>
       </Modal>
     </>
